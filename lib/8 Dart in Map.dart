f8b0e5316405feb7b2a4a09aabf67e932ee08f7e
Hasan <bdhasan009@gmail.com>
2023-01-09 13:53:31 +0600

    8.1 dart in Map

---
+++ lib/8 Dart in Map.dart
@@ -0,0 +1,13 @@
+// dart in Map
+
+void main(){
+
+  Map myMap = {
+    "first_name" : "Hasan",
+    "second_name" : "tanbir",
+    "third_name" : "Jannat"
+  };
+  
+  print(myMap["third_name"]);
+  
+}
